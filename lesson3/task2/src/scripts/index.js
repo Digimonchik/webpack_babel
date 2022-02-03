2309aaf4dcd95831a1f8e43580fde6e3f8b34d95
Digimonchik <da.logvinenko@gmail.com>
2022-02-03 18:29:45 +0300

    update

---
+++ lesson3/task2/src/scripts/index.js
@@ -0,0 +1,21 @@
+import { initToDoHandlers } from "./initHandlers.js";
+import { createCheckbox, renderTasks } from "./addTask.js";
+import { getTasksList } from "./tasksGateway.js";
+import { setItem } from "./common.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  getTasksList().then((taskList) => {
+    setItem("tasksList", taskList);
+    renderTasks();
+  });
+  initToDoHandlers();
+  console.log(createCheckbox({ done: false, id: "1" }));
+});
+
+const onStorangeChange = (e) => {
+  console.log(e.key);
+  if (e.key === "tasksList") {
+    renderTasks();
+  }
+};
+window.addEventListener("storage", onStorangeChange);
