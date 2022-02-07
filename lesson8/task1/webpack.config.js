4a1bd5b9655520a5fd7c879b3bb5ba59207edf89
Digimonchik <da.logvinenko@gmail.com>
2022-02-07 18:50:40 +0300

    update

--- lesson8/task1/webpack.config.js
+++ lesson8/task1/webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode = 'production';
+  const isProduction = argv.mode == 'production';
   const config = {
   entry: "./src/index.js",
   output: {
