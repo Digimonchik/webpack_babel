eac0388f15e5ab10ca6dd1b8da1e1633df56e305
Digimonchik <da.logvinenko@gmail.com>
2022-02-07 19:26:54 +0300

    update

---
+++ lesson8/task2/src/tasksGateway.js
@@ -0,0 +1,29 @@
+const baseUrl = "https://61e1672f63f8fc0017618b99.mockapi.io/api/v1/tasks";
+
+export const getTasksList = () =>
+  fetch(baseUrl)
+    .then((response) => response.json())
+    .then((tasks) => tasks);
+
+export const createTask = (taskData) =>
+  fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(taskData),
+  });
+
+export const updateTask = (taskId, updatedTaskData) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+
+export const deleteTask = (taskId) =>
+  fetch(`${baseUrl}/${taskId}`, {
+    method: "DELETE",
+  });
